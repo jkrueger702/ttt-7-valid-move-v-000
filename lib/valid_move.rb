9baddbccbf2ae8c058a15767f91ff57b0eb93ba8
Jordan Krueger <jkrueger702@gmail.com>
2018-05-03 05:26:24 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,20 @@
-def valid_move
-  if position_taken? between?(0, 8)
-    true
-  elsif
+#def valid_move
+#  if position_taken? between?(0, 8)
+#    true
+#  elsif
+#    false
+#  end
+#end
+def valid_move?(board, index)
+  if index > 0
+    index-1
+  end
+  if index > 8
     false
+  elsif position_taken?(board, index)
+    false
+  else
+    true
   end
 end
 
